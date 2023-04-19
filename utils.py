38c905f62c54498a7f503dae3c9e8029eb100db4
Seyed Mohammad Hossein Hosseini <49597221+smh-hosseiny@users.noreply.github.com>
2023-04-19 14:45:48 -0400

    Update utils.py
    updated utils.

--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
 import torch.nn as nn
+import torch
 import torch.nn.functional as F
+from collections import namedtuple
+import numpy as np
 
 class ConvBlock(nn.Module):
-    """A layer that applies a convolution followed by an activation function."""
-
     def __init__(self, in_channels, out_channels):
         super(ConvBlock, self).__init__()
         self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
@@ -37,13 +38,200 @@ class Conv3x3(nn.Module):
 
 
 
+def upsample(x):
+    return F.interpolate(x, scale_factor=2, mode="nearest")
 
-class Upsample(nn.Module):
 
-    def __init__(self, scale_factor=2, mode='nearest'):
-        super(Upsample, self).__init__()
-        self.scale_factor = scale_factor
-        self.mode = mode
 
-    def forward(self, x):
-        return F.interpolate(x, scale_factor=self.scale_factor, mode=self.mode)
+class BackprojectDepth(nn.Module):
+
+    def __init__(self, batch_size, height, width):
+        super(BackprojectDepth, self).__init__()
+
+        self.batch_size = batch_size
+        self.height = height
+        self.width = width
+
+        # Create the meshgrid
+        x_coords = torch.arange(self.width, dtype=torch.float32)
+        y_coords = torch.arange(self.height, dtype=torch.float32)
+        self.id_coords = torch.stack(torch.meshgrid(x_coords, y_coords), dim=0)
+
+        # Create the ones tensor
+        self.ones = torch.ones(self.batch_size, 1, self.height * self.width, dtype=torch.float32)
+
+        # Create the pixel coordinates tensor
+        self.pix_coords = torch.stack([self.id_coords[0].view(-1), self.id_coords[1].view(-1)], dim=0)
+        self.pix_coords = self.pix_coords.repeat(self.batch_size, 1, 1)
+        self.pix_coords = torch.cat([self.pix_coords, self.ones], dim=1)
+
+    def forward(self, depth, inv_K):
+        cam_points = torch.matmul(inv_K[:, :3, :3], self.pix_coords)
+
+        cam_points = depth.view(self.batch_size, 1, -1) * cam_points
+        cam_points = torch.cat([cam_points, self.ones], dim=1)
+
+        return cam_points
+
+
+class Project3D(nn.Module):
+    def __init__(self, batch_size, height, width, eps=1e-7):
+        super(Project3D, self).__init__()
+        self.batch_size = batch_size
+        self.height = height
+        self.width = width
+        self.eps = eps
+
+    def forward(self, points, K, T):
+        P = K[:, :3, :] @ T
+
+        cam_points = P @ points
+
+        z = cam_points[:, 2, :] + self.eps
+        pix_coords = cam_points[:, :2, :] / z.unsqueeze(1)
+        next_depth = z.unsqueeze(1).view(self.batch_size, 1, self.height, self.width)
+
+        pix_coords = pix_coords.view(self.batch_size, 2, self.height, self.width)
+        pix_coords = pix_coords.permute(0, 2, 3, 1)
+        pix_coords[..., 0] /= (self.width - 1) / 2
+        pix_coords[..., 1] /= (self.height - 1) / 2
+        pix_coords = pix_coords - 1
+        return pix_coords, next_depth
+
+
+class SSIM(nn.Module):
+
+    def __init__(self):
+        super(SSIM, self).__init__()
+
+        self.refl = nn.ReflectionPad2d(1)
+
+        self.C1 = 0.01 ** 2
+        self.C2 = 0.03 ** 2
+
+    def forward(self, x, y):
+        x = self.refl(x)
+        y = self.refl(y)
+
+        mu_x = nn.functional.avg_pool2d(x, kernel_size=3, stride=1, padding=0)
+        mu_y = nn.functional.avg_pool2d(y, kernel_size=3, stride=1, padding=0)
+
+        sigma_x = nn.functional.avg_pool2d(x ** 2, kernel_size=3, stride=1, padding=0) - mu_x ** 2
+        sigma_y = nn.functional.avg_pool2d(y ** 2, kernel_size=3, stride=1, padding=0) - mu_y ** 2
+        sigma_xy = nn.functional.avg_pool2d(x * y, kernel_size=3, stride=1, padding=0) - mu_x * mu_y
+
+        SSIM_n = (2 * mu_x * mu_y + self.C1) * (2 * sigma_xy + self.C2)
+        SSIM_d = (mu_x ** 2 + mu_y ** 2 + self.C1) * (sigma_x + sigma_y + self.C2)
+
+        return torch.clamp((1 - SSIM_n / SSIM_d) / 2, 0, 1)
+    
+
+
+def readlines(filename):
+    with open(filename, 'r') as f:
+        return f.read().splitlines()
+    
+
+def normalize_image(x):
+    return (x - x.min()) / (x.max() - x.min() + 1e-5)
+
+
+palette = [128, 64, 128, 244, 35, 232, 70, 70, 70, 102, 102, 156, 190, 153, 153,
+           153, 153, 153, 250, 170, 30,
+           220, 220, 0, 107, 142, 35, 152, 251, 152, 70, 130, 180, 220, 20, 60,
+           255, 0, 0, 0, 0, 142, 0, 0, 70,
+           0, 60, 100, 0, 80, 100, 0, 0, 230, 119, 11, 32]
+
+
+def decode_seg_map(seg_map, num_classes=19):
+    input_shape = seg_map.shape
+    seg_map = seg_map.flatten()
+    empty = torch.stack([torch.zeros(seg_map.shape) for _ in range(3)])
+
+    for i in torch.unique(seg_map).data.cpu():
+        i = int(i)
+        seg_map = seg_map.flatten()
+
+        mask = seg_map == i
+        for c in range(3):
+            empty[c][mask] = palette[3 * i + c]
+    empty = empty.view(3, *input_shape[1:])
+    return empty.byte()
+
+
+Label = namedtuple('Label', [
+
+    'name',  # The identifier of this label, e.g. 'car', 'person', ... .
+    # We use them to uniquely name a class
+
+    'id',  # An integer ID that is associated with this label.
+    # The IDs are used to represent the label in ground truth images
+    # An ID of -1 means that this label does not have an ID and thus
+    # is ignored when creating ground truth images (e.g. license plate).
+    # Do not modify these IDs, since exactly these IDs are expected by the
+    # evaluation server.
+
+    'trainId',  # Feel free to modify these IDs as suitable for your method. Then create
+    # ground truth images with train IDs, using the tools provided in the
+    # 'preparation' folder. However, make sure to validate or submit results
+    # to our evaluation server using the regular IDs above!
+    # For trainIds, multiple labels might have the same ID. Then, these labels
+    # are mapped to the same class in the ground truth images. For the inverse
+    # mapping, we use the label that is defined first in the list below.
+    # For example, mapping all void-type classes to the same ID in training,
+    # might make sense for some approaches.
+    # Max value is 255!
+
+    'category',  # The name of the category that this label belongs to
+
+    'categoryId',  # The ID of this category. Used to create ground truth images
+    # on category level.
+
+    'hasInstances',  # Whether this label distinguishes between single instances or not
+
+    'ignoreInEval',  # Whether pixels having this class as ground truth label are ignored
+    # during evaluations or not
+
+    'color',  # The color of this label
+])
+labels = [
+    #       name                     id    trainId   category            catId     hasInstances   ignoreInEval   color
+    Label('unlabeled', 0, 255, 'void', 0, False, True, (0, 0, 0)),
+    Label('ego vehicle', 1, 255, 'void', 0, False, True, (0, 0, 0)),
+    Label('rectification border', 2, 255, 'void', 0, False, True, (0, 0, 0)),
+    Label('out of roi', 3, 255, 'void', 0, False, True, (0, 0, 0)),
+    Label('static', 4, 255, 'void', 0, False, True, (0, 0, 0)),
+    Label('dynamic', 5, 255, 'void', 0, False, True, (111, 74, 0)),
+    Label('ground', 6, 255, 'void', 0, False, True, (81, 0, 81)),
+    Label('road', 7, 0, 'flat', 1, False, False, (128, 64, 128)),
+    Label('sidewalk', 8, 1, 'flat', 1, False, False, (244, 35, 232)),
+    Label('parking', 9, 255, 'flat', 1, False, True, (250, 170, 160)),
+    Label('rail track', 10, 255, 'flat', 1, False, True, (230, 150, 140)),
+    Label('building', 11, 2, 'construction', 2, False, False, (70, 70, 70)),
+    Label('wall', 12, 3, 'construction', 2, False, False, (102, 102, 156)),
+    Label('fence', 13, 4, 'construction', 2, False, False, (190, 153, 153)),
+    Label('guard rail', 14, 255, 'construction', 2, False, True, (180, 165, 180)),
+    Label('bridge', 15, 255, 'construction', 2, False, True, (150, 100, 100)),
+    Label('tunnel', 16, 255, 'construction', 2, False, True, (150, 120, 90)),
+    Label('pole', 17, 5, 'object', 3, False, False, (153, 153, 153)),
+    Label('polegroup', 18, 255, 'object', 3, False, True, (153, 153, 153)),
+    Label('traffic light', 19, 6, 'object', 3, False, False, (250, 170, 30)),
+    Label('traffic sign', 20, 7, 'object', 3, False, False, (220, 220, 0)),
+    Label('vegetation', 21, 8, 'nature', 4, False, False, (107, 142, 35)),
+    Label('terrain', 22, 9, 'nature', 4, False, False, (152, 251, 152)),
+    Label('sky', 23, 10, 'sky', 5, False, False, (70, 130, 180)),
+    Label('person', 24, 11, 'human', 6, True, False, (220, 20, 60)),
+    Label('rider', 25, 12, 'human', 6, True, False, (255, 0, 0)),
+    Label('car', 26, 13, 'vehicle', 7, True, False, (0, 0, 142)),
+    Label('truck', 27, 14, 'vehicle', 7, True, False, (0, 0, 70)),
+    Label('bus', 28, 15, 'vehicle', 7, True, False, (0, 60, 100)),
+    Label('caravan', 29, 255, 'vehicle', 7, True, True, (0, 0, 90)),
+    Label('trailer', 30, 255, 'vehicle', 7, True, True, (0, 0, 110)),
+    Label('train', 31, 16, 'vehicle', 7, True, False, (0, 80, 100)),
+    Label('motorcycle', 32, 17, 'vehicle', 7, True, False, (0, 0, 230)),
+    Label('bicycle', 33, 18, 'vehicle', 7, True, False, (119, 11, 32)),
+    Label('license plate', -1, -1, 'vehicle', 7, False, True, (0, 0, 142)),
+]
+
+
+
